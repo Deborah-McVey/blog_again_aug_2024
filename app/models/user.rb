2e43401cf19f64188448c5d823c9c519547184d0
Deborah McVey <deborah.mcvey@gmail.com>
2024-08-23 13:34:17 -0400

    hashed password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    has_one :profile
+    validates :email, uniqueness: true, length: { in: 5..50 }, format: { with: /\A[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}\z/i }
+    validates :password, confirmation: true
 
+    has_one :profile
     has_many :articles, -> { order 'published_at DESC, title ASC' }, dependent: :nullify
     has_many :replies, through: :articles, source: :comments
 end
